6cd6e749fea915f9a07ce769012ab50c4bb8e86a
Масленников Владислав <vmaslennikov@mkit.local>
2026-03-18 12:20:57 +0300

    123

--- src/App.js
+++ src/App.js
@@ -1,36 +1,32 @@
-import { useState } from 'react';
-import Alchemy from './components/Alchemy';
+import { useRef, useState } from 'react';
 import './styles/style.css'
-import CenterDivModal from './components/CenterDivModal';
-import TicTacToe from './components/TicTacToe';
-import FindSecret from './components/FindSecret';
+import './assets/burger_menu.svg'
 import Map from './components/Map';
 
 function App() {
-  const [alchemy, setAlchemy] = useState(false)
-  const [centerDiv, setCenterDiv] = useState(false)
-  const [ticTacToe, setTicTacToe] = useState(false)
-  const [findSecret, setFindSecret] = useState(false)
   const [mapIsOpen, setMapIsOpen] = useState(false)
+  const mapBtnRef = useRef(null)
   
+  const toggleBtn = () => {
+    if (!mapIsOpen) {
+    if (mapBtnRef.current) {
+      mapBtnRef.current.style.display = 'none';
+    }
+    setMapIsOpen(true);
+  } else {
+    setMapIsOpen(false);
+  }
+  }
+
   return (
     <div className='map'>
-
-      {/* <button onClick={()=> setAlchemy(true)}> Alchemy</button>
-      <Alchemy isOpen={alchemy} onClose={() => setAlchemy(false)}/>
-
-      <button onClick={()=> setCenterDiv(true)}> centerDiv</button>
-      <CenterDivModal isOpen={centerDiv} onClose={() => setCenterDiv(false)}/>
-
-      <button onClick={()=> setTicTacToe(true)}> ticTacToe</button>
-      <TicTacToe isOpen={ticTacToe} onClose={() => setTicTacToe(false)}/>
-
-      <button onClick={()=> setFindSecret(true)}> findSecret</button>
-      <FindSecret isOpen={findSecret} onClose={() => setFindSecret(false)}/> */}
-
-      <button onClick={()=> setMapIsOpen(true)}> Map</button>
+      <button 
+        ref={mapBtnRef}
+        className='map_icon' 
+        onClick={toggleBtn}
+        >
+      </button>
       <Map isOpen={mapIsOpen} onClose={() => setMapIsOpen(false)} />
-
     </div>
   );
 }
--- src/components/Map.js
+++ src/components/Map.js
@@ -4,10 +4,6 @@ import goal_1 from '../assets/goals/goal 1.svg'
 import goal_2 from '../assets/goals/goal 2.svg'
 import goal_3 from '../assets/goals/goal 3.svg'
 import goal_4 from '../assets/goals/goal 4.svg'
-//roads
-// import first from '../assets/road/first.svg'
-// import second from '../assets/road/second.svg'
-// import third from '../assets/road/third.svg'
 
 function Map({isOpen, onClose}) {
   if(!isOpen) return null;
@@ -15,20 +11,11 @@ function Map({isOpen, onClose}) {
     <div className="map_container">
     <h1>Дорожная карта</h1>
         <div className='goalContainer'>
-                <img src={goal_1} id='gl1'/>
-                <img src={goal_3} id='gl3'/>
-                <img src={goal_4} id='gl4'/>
-                <img src={goal_2} id='gl2'/>
-                
-                {/* <img src={first} id='rd1'/>
-                <img src={second} id='rd2'/>
-                <img src={third} id='rd3'/> */}
+                <img src={goal_1} id='gl1' alt='Флажок #1'/>
+                <img src={goal_3} id='gl3' alt='Флажок #2'/>
+                <img src={goal_4} id='gl4' alt='Флажок #3'/>
+                <img src={goal_2} id='gl2' alt='Флажок #4'/>
         </div>
-        <button 
-            onClick={onClose}
-            className='backward_btn'
-        > ←
-        </button>
     </div>
   );
 }
